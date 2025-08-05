a5e1e959a0a9ff4f156c910f2033b45be63e9d43
saikatportfolio <saikatportfolio@users.noreply.github.com>
2025-08-05 07:04:36 +0000

    deploy: 11c380dea287a399b609d8a2782a02c515b78aaf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c2988dae649e9b4809e50de8a940503b
 "assets/packages/mixpanel_flutter/assets/mixpanel.js": "48a5241deca6bca875259d2b562d7081",
 "version.json": "389a6e6e6f34312743ca5f36467c84b5",
 "manifest.json": "b19362383014fc8ebdd964dcfcb0c617",
-"flutter_bootstrap.js": "f68d24e57f07fb00151ba249809409f0",
+"flutter_bootstrap.js": "b025b76c48044deb95a4df490ccdd20d",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
