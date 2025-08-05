1bb4bfca57eddb0ba7d5bfb5219abe5acb9025e9
saikatportfolio <saikatportfolio@users.noreply.github.com>
2025-08-05 19:02:35 +0000

    deploy: 536bd3a74a8c4a1ae0ebf717b4a9d7e6e27f4e83

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,15 +3,15 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "8cfe04b496c928288051b0878c02f40d",
+const RESOURCES = {"assets/AssetManifest.bin": "5f220a2d0ce0512dfec8132143c42ebd",
 "assets/assets/images/google_logo.png": "36807175c6e8f9441a3d31f2882d0dcf",
 "assets/assets/images/nutrition_guide_ckd.jpeg": "82faf7792da5548d6cd941a1343fe0d9",
 "assets/assets/images/kidney_health.png": "edf43478b45e04ffc30e28705837c251",
 "assets/assets/images/onboarding3.png": "058ef63cfec2cd226b5b8b37d5529335",
+"assets/assets/images/understand_ckd.png": "89ce8a3d2ba5dad34783f4e4ac3ba1a9",
 "assets/assets/images/patient_image.png": "d879c79ad335ebb5e69ed66a8b193831",
 "assets/assets/images/feedback.jpg": "7d11169619134299c2c0fd7056dec27e",
 "assets/assets/images/auth_image.png": "f6fe275419eda3244f6568e3c8add1d7",
-"assets/assets/images/educate_ckd.png": "89ce8a3d2ba5dad34783f4e4ac3ba1a9",
 "assets/assets/images/gfr.png": "461fa54ac71c06657b83f24ee7adeed5",
 "assets/assets/images/onboarding1.png": "32075da5f17e1779b7fd5bab3eecf6a2",
 "assets/assets/images/dietician.jpg": "d49a2a2e3d8b1c3319b68ab7519adb96",
@@ -20,15 +20,15 @@ const RESOURCES = {"assets/AssetManifest.bin": "8cfe04b496c928288051b0878c02f40d
 "assets/assets/images/your_meal.jpg": "cf86dd52f054eab1b03dcd33322201d2",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/AssetManifest.json": "86ef60c9948fe490fc703efca5eb1c5d",
+"assets/AssetManifest.json": "af289fe0941120e5e2cf0031fde527c0",
 "assets/fonts/MaterialIcons-Regular.otf": "cacf7b81687d78000ac4c6f32ef7b51f",
-"assets/AssetManifest.bin.json": "fe37c49c35d08d19d2442d0927f5a271",
+"assets/AssetManifest.bin.json": "22b143411bb842ca7be4af82b3ff0441",
 "assets/NOTICES": "32dc035a6dc82c3cb9277fa211fb0e45",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/packages/mixpanel_flutter/assets/mixpanel.js": "48a5241deca6bca875259d2b562d7081",
 "version.json": "389a6e6e6f34312743ca5f36467c84b5",
 "manifest.json": "b19362383014fc8ebdd964dcfcb0c617",
-"flutter_bootstrap.js": "212601459fd330e89efa95801c2ba421",
+"flutter_bootstrap.js": "d36e3f64815eb48959efa65ec19679c8",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
@@ -46,7 +46,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8cfe04b496c928288051b0878c02f40d
 "icons/Icon-192.png": "94a93d6a62c727a2cd3470bb923d8024",
 "icons/Icon-maskable-512.png": "535019822474ec5a8d55849e624ca3a4",
 "icons/Icon-maskable-192.png": "5852bac58492f16f3178118a6d91f856",
-"main.dart.js": "dd6fc765ea4847ad877ffb18d9e5ed97"};
+"main.dart.js": "d8491866f1c7efbb75ae7c31333b35c4"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
