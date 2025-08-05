c9fc2d5b5ee0e11f01c5a80b47b24282f9570f93
Saikat Sengupta <saikat.sengupta@harman.com>
2025-08-05 10:16:47 +0530

    Triggered a new build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "123476038"
+    serviceWorkerVersion: "3743463609"
   }
 });
