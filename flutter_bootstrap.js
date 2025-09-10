2ef9cf0ae067b80b737d91e24a58a38242d7c6ae
saikatportfolio <saikatportfolio@users.noreply.github.com>
2025-09-10 08:21:05 +0000

    deploy: db66fd6e654476e7b782b873cee1056c7368339c

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1070806246"
+    serviceWorkerVersion: "1650240585"
   }
 });
